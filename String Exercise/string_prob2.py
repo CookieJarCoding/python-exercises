6f1924e58cad55f96d6ab89abf52c2af15622158
jllantero <jllantero@gmail.com>
2020-08-20 12:37:06 +0800

    Changed the variable name
    'temp' became 'first_letter'

--- String Exercise/string_prob2.py
+++ String Exercise/string_prob2.py
@@ -1,8 +1,8 @@
 print("\n[STRINGS - PROBLEM 2]\n")
 string = input("Enter string: ")
 
-temp = string[0]
-string = string.replace(temp, '$')
-string = temp + string[1:]
+first_letter = string[0]
+string = string.replace(first_letter, '#')
+string = first_letter + string[1:]
 
 print(f'\nOUTPUT >> {string}\n')
