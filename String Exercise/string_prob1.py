17d18ea313379b07a53c03b512356f19b14c83c8
jllantero <jllantero@gmail.com>
2020-08-20 12:42:01 +0800

    added comments

--- String Exercise/string_prob1.py
+++ String Exercise/string_prob1.py
@@ -1,9 +1,12 @@
 print("\n[STRINGS - PROBLEM 1]\n")
 
-string1 = raw_input("Enter first string: ")
-string2 = raw_input("Enter second string: ")
+# take the two input strings from the user
+string1 = input("Enter first string: ")
+string2 = input("Enter second string: ")
 
+# replace the last two letters of the two strings
 new1 = string1[:-2] + string2[-2:]
 new2 = string2[:-2] + string1[-2:]
 
+# display the result
 print('\nOUTPUT >> %s %s\n'%(new1, new2))
--- String Exercise/string_prob2.py
+++ String Exercise/string_prob2.py
@@ -1,8 +1,14 @@
 print("\n[STRINGS - PROBLEM 2]\n")
+
+# take a string from the user
 string = input("Enter string: ")
 
+# save the first letter of this string
 first_letter = string[0]
+
+# replace all the succeeding repetitions of the first letter
 string = string.replace(first_letter, '#')
 string = first_letter + string[1:]
 
+# display the result
 print(f'\nOUTPUT >> {string}\n')
