bc1d72334ff9b2a872d5a2684a12d65e7e11979f
jllantero <jllantero@gmail.com>
2020-08-22 20:45:36 +0800

    added the solution to the calculator exercise

---
+++ Calculator/calculator.py
@@ -0,0 +1,23 @@
+print("\n[CALCULATOR]\n")
+
+# ask the user for two numbers
+first_num = int(input('Enter the first number: '))
+second_num = int(input('Enter the second number: '))
+
+# perform the addition and assign the result to the variable sum
+sum = first_num + second_num
+
+# perform the subtraction and assign the result to the variable difference
+difference = first_num - second_num
+
+# perform the multiplication and assign the result to the variable product
+product = first_num * second_num
+
+# perform the division and assign the result to the variable quotient
+quotient = first_num / second_num
+
+# display the result of the operations performed
+print('\nsum >> ' + str(sum))
+print('difference >> ' + str(difference))
+print('product >> ' + str(product))
+print('quotient >> ' + str(quotient) + '\n')
